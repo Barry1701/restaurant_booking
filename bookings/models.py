9dc99fd189cbcb9d6b9bc9798b57f5ac82f4c7bf
Bartosz Stanczuk <synergy170185@gmail.com>
2024-04-17 14:54:00 +0000

    Add a Booking model to our project

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Booking(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    number_of_guests = models.PositiveIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Booking #{self.pk} - {self.date} at {self.time} for {self.number_of_guests} guests"
+
+    
